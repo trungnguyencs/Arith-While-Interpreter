976d00d75d31221b6de6fe48012b8cecc9f0fb33
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2019-04-17 16:20:50 -0700

    completed

---
+++ WHILE-interpreter/README.md
@@ -0,0 +1,34 @@
+# Programming Assignment 2 - The WHILE language:
+
+For the input AST of an arithmetic expression, implement an interpreter in the form of a function called "eval" which takes in the AST  and returns the evaluated result for that expression
+
+## New feature: 
+
+We have added the following operators in addition to the required operators:
+- Div
+- Mod
+- GreaterEq
+- SmallerEq
+
+## Running the file
+
+$node While.js
+
+## Test cases
+
+Test case Assign: 'x = 5;' evaluates to:  { x: 5 } 
+
+Test case Seq: '(x = 5; y = 10); z = 15;' evaluates to:  { x: 5, y: 10, z: 15 } 
+
+Test case Skip: '(x = 5; skip();); y = 10;' evaluates to:  { x: 5, y: 10 }
+
+Test case If1: 'true ? z = 15 : y = 10;' evaluates to:  { z: 15 }
+
+Test case If2: '(x > 5) ? z = 15 : x = 100;' evaluates to:  { x: 100 }
+
+Test case While1: 'i = 0; while(i < 5){i++;}' evaluates to:  { i: 5 }
+
+Test case While2: 'i = 0; j = 0; while(i < 10){i\%3 == 0) ? j++ : skip(); i++}' evaluates to:  { i: 10, j: 4 }
+
+## Authors
+Trung Nguyen (tnguy404) and Geetanjali Rakshit (grakshit) worked together for 6 hours
--- WHILE-interpreter/While.js
+++ WHILE-interpreter/While.js
@@ -1,3 +1,16 @@
+// Programming Assignment 2 - The WHILE language:
+// In Haskell (or the language of your choice), write an interpreter for the WHILE language 
+// (slides on the WHILE language are available on the course webpage -> resources -> slides ).  
+// Your program should consist of:
+// - A data structure for the abstract syntax tree (AST) of WHILE. You can decide on an appropriate representation (e.g. strings) for variables.
+// - An interpreter for this AST. You should choose a suitable representation for stores. 
+//   Your interpreter should include functions to evaluate arithmetic expressions, boolean expressions, and commands.
+// - Test cases which show that your AST and interpreter work.  These test cases should show good code coverage (i.e. test all cases)
+// - Finally, add a feature to your language.  This addition will involve modifying the AST and interpreter to support this new feature.
+// Good test cases should make you (and me, the grader) feel confident that your program works.  Good luck!  
+// Note: Extra credit for HW 2 if you have used 2 or more languages among homework 1 and 2.  
+// Please, note in your README what language you used for each homework.
+
 // ----------------------------
 // Arithmetic
 // ----------------------------
@@ -17,13 +30,6 @@ function Var(v){
         return s[this.v];
     }
 }
-function Mul(e1, e2){
-    this.e1 = e1;
-    this.e2 = e2;
-    this.eval = function(s){
-        return this.e1.eval(s) * this.e2.eval(s);
-    }
-}
 function Sum(e1, e2){
     this.e1 = e1;
     this.e2 = e2;
@@ -38,11 +44,33 @@ function Minus(e1, e2){
         return this.e1.eval(s) - this.e2.eval(s);
     }
 }
-
+function Mul(e1, e2){
+    this.e1 = e1;
+    this.e2 = e2;
+    this.eval = function(s){
+        return this.e1.eval(s) * this.e2.eval(s);
+    }
+}
+function Div(e1, e2){
+    this.e1 = e1;
+    this.e2 = e2;
+    this.eval = function(s){
+        return this.e1.eval(s) / this.e2.eval(s);
+    }
+}
+function Mod(e1, e2){
+    this.e1 = e1;
+    this.e2 = e2;
+    this.eval = function(s){
+        return this.e1.eval(s) % this.e2.eval(s);
+    }
+}
 Int.prototype = new Aexp();
 Sum.prototype = new Aexp();
-Mul.prototype = new Aexp();
 Minus.prototype = new Aexp();
+Mul.prototype = new Aexp();
+Div.prototype = new Aexp();
+Mod.prototype = new Aexp();
 
 // ----------------------------
 // Boolean
@@ -84,6 +112,20 @@ function Greater(b1, b2){
         return this.b1.eval(s) > this.b2.eval(s);
     }
 }
+function SmallerEq(b1, b2){
+    this.b1 = b1;
+    this.b2 = b2;
+    this.eval = function(s){
+        return this.b1.eval(s) <= this.b2.eval(s);
+    }
+}
+function GreaterEq(b1, b2){
+    this.b1 = b1;
+    this.b2 = b2;
+    this.eval = function(s){
+        return this.b1.eval(s) >= this.b2.eval(s);
+    }
+}
 function And(b1, b2) {
     this.b1 = b1;
     this.b2 = b2;
@@ -98,12 +140,13 @@ function Or(b1, b2) {
         return this.b1.eval(s) || this.b2.eval(s);
     }
 }
-
 Bool.prototype = new Bexp();
 Not.prototype = new Bexp();
 Equal.prototype = new Bexp();
 Smaller.prototype = new Bexp();
 Greater.prototype = new Bexp();
+SmallerEq.prototype = new Bexp();
+GreaterEq.prototype = new Bexp();
 And.prototype = new Bexp();
 Or.prototype = new Bexp();
 
@@ -128,7 +171,7 @@ function Seq(c1, c2){
 			return s2;
 	}
 }
-function Assign(varName, e){
+function Asgn(varName, e){
 	this.varName = varName;
 	this.e = e;
 	this.eval = function(s){
@@ -153,51 +196,67 @@ function While(b, c){
 	this.eval = function(s){
 	if(this.b.eval(s)){
         s1 = c.eval(s);
-        s2 = While(b,c).eval(s1);
+        s2 = new While(b,c).eval(s1);
 		return s2;
 	}
 	else
 		return s;
 	}
 }
-
 Skip.prototype = new Comm();
 Seq.prototype = new Comm();
-Assign.prototype = new Comm();
+Asgn.prototype = new Comm();
 If.prototype = new Comm();
 While.prototype = new Comm();
-And.prototype = new Comm();
-Or.prototype = new Comm();
 
-var s = {};
-testcase1 = new Mul(new Sum(new Int(5), new Int(3)), new Int(2));
-console.log(testcase1.eval(s) == 17);
-testcase2 = new Or(new Smaller(new Bool(7), new Bool(5)), new Bool(true));
-console.log(testcase2.eval(s));
+// ----------------------------
+// Test cases
+// ----------------------------
+var_x = new Var('x');
+asgn_x = new Asgn('x', new Int(5));
+asgn_y = new Asgn('y', new Int(10));
+asgn_z = new Asgn('z', new Int(15));
+asgn_x1 = new Asgn('x', new Int(100));
 
-c1 = new Var('x');
-c2 = new Int(5);
-c3 = new Assign('x',new Int(5));
-// ifC1 = new If(new Greater(c1, 4), new Assign(new Var('y'), 5));
-console.log(c1.eval(s));
-console.log(c2.eval(s));
-console.log(c3.eval(s));
-// console.log(ifC1.eval(s))
+// Test case for Assign():
+tcAsgn = asgn_x;
+// Test case for Seq():
+tcSeq = new Seq(new Seq(asgn_x, asgn_y), asgn_z);
+// Test case for Skip():
+tcSkip = new Seq(new Seq(asgn_x, new Skip()), asgn_y);
+// Test case 1 for If():
+tcIf1 = new If(new Bool(true), asgn_z, asgn_y);
+// Test case 2 for If():
+tcIf2 = new If(new Greater(asgn_x, new Int(6)), asgn_z, asgn_x1);
+// Test case 1 for While():
+While1_cond = new Smaller(new Var('i'), new Int(5)); 
+While1_exe = new Asgn('i', new Sum(new Var('i'), new Int(1)));
+While1 = new While(While1_cond, While1_exe);
+tcWhile1 = new Seq(new Asgn('i', new Int(0)), While1);
+// Test case 2 for While():
+While2_cond = new Smaller(new Var('i'), new Int(10)); 
+If_cond = new Equal(new Mod(new Var('i'), new Int(3)), new Int(0));
+If_exe = new Asgn('j', new Sum(new Var('j'), new Int(1)));
+While2_exe1 = new If(If_cond, If_exe, new Skip());
+While2_exe2 = new Asgn('i', new Sum(new Var('i'), new Int(1)));
+While2_exe = new Seq(While2_exe1, While2_exe2);
+While2 = new While(While2_cond, While2_exe);
+tcWhile2 = new Seq(new Seq(new Asgn('i', new Int(0)), new Asgn('j', new Int(0))), While2);
 
-// c1 = Assign('i',IntExp(0))
-// c2 = Assign('x', IntExp(0))
-// ifC1_d1 = Assign('x', MinExp(DivExp(MulExp(SumExp(VarExp('x'), VarExp('i')), IntExp(4)), IntExp(2)), IntExp(-2)))
-// ifC1_d2 = Assign('x', DivExp(MulExp(SumExp(VarExp('x'), VarExp('i')), IntExp(4)), IntExp(2)))
-// ifC1 = If(Equal(ModExp(VarExp('i'), IntExp(2)), IntExp(0)), ifC1_d1, ifC1_d2)
-// ifC2_d1 = Assign('y', IntExp(3))
-// ifC2_d2 = Skip()
-// ifC2 = If(Great(IntExp(3), IntExp(1)), ifC2_d1, ifC2_d2)
-// whileC = Next(Next(ifC1, Assign('i', SumExp(VarExp('i'), IntExp(1)))), ifC2)
-// c3 = While(Less(VarExp('i'), IntExp(5)), whileC)
-// testCase1 = Next(Next(Next(c1, Skip()),c2), c3)
-// s = dict()
-// c3 = While(Less(VarExp('i'), IntExp(5)), whileC)
-// tc0 =  Next(Next(Next(c1, Skip()),c2), c3)
-// print tc0.eval(s)
-// print type(tc0).__name__!="Next"
-// print type('Skip')
+stmtAsgn = "x = 5;";
+stmtSeq = "(x = 5; y = 10); z = 15;";
+stmtSkip = "(x = 5; skip();); y = 10;";
+stmtIf1 = "true ? z = 15 : y = 10;";
+stmtIf2 = "(x > 5) ? z = 15 : x = 100;";
+stmtWhile1 = "i = 0; while(i < 5){i++;}";
+stmtWhile2 = "i = 0; j = 0; while(i < 10){i%3 == 0) ? j++ : skip(); i++}";
+
+console.log('----------------------------------------------------------------------------');
+var s = {}; console.log("Test case Assign: '" + stmtAsgn + "' evaluates to: ", tcAsgn.eval(s));
+var s = {}; console.log("Test case Seq: '" + stmtSeq + "' evaluates to: ", tcSeq.eval(s));
+var s = {}; console.log("Test case Skip: '" + stmtSkip + "' evaluates to: ", tcSkip.eval(s));
+var s = {}; console.log("Test case If1: '" + stmtIf1 + "' evaluates to: ", tcIf1.eval(s));
+var s = {}; console.log("Test case If2: '" + stmtIf2 + "' evaluates to: ", tcIf2.eval(s));
+var s = {}; console.log("Test case While1: '" + stmtWhile1 + "' evaluates to: ", tcWhile1.eval(s));
+var s = {}; console.log("Test case While2: '" + stmtWhile2 + "' evaluates to: ", tcWhile2.eval(s));
+console.log('----------------------------------------------------------------------------');
